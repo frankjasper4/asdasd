506f57113df752bcc9503ecbbfe98dac8de000ba
N3002 User <N3002user@N3002.uc-bcf.edu.ph>
2024-02-21 09:50:42 +0800

    Controller Created (Defined the Products)

--- laravel_app/app/Http/Controllers/ProductController.php
+++ laravel_app/app/Http/Controllers/ProductController.php
@@ -1,4 +1,3 @@
-/ ProductController.php
 <?php
 
 namespace App\Http\Controllers;
@@ -8,9 +7,21 @@ class ProductController extends Controller
     public function index()
     {
         $products = [
-            ['name' => 'Product 1', 'price' => 19.99],
-            ['name' => 'Product 2', 'price' => 29.99],
-            // Add more products as needed
+            ['iPhone' => 'Product 1', 'price' => 999.99],
+            ['Samsung' => 'Product 2', 'price' => 899.99],
+            ['Oppo' => 'Product 2', 'price' => 499.99],
+            ['Xiaomi' => 'Product 2', 'price' => 399.99],
+            ['Vivo' => 'Product 2', 'price' => 299.99],
+            ['Redmi' => 'Product 2', 'price' => 199.99],
+            ['ROG' => 'Product 2', 'price' => 1599.99],
+            ['PC Set 1' => 'Product 2', 'price' => 499.99],
+            ['PC Set 2' => 'Product 2', 'price' => 599.99],
+            ['PC Set 3' => 'Product 2', 'price' => 699.99],
+            ['Laptop 1' => 'Product 2', 'price' => 399.99],
+            ['Laptop 2' => 'Product 2', 'price' => 499.99],
+            ['Laptop 3' => 'Product 2', 'price' => 599.99],
+            
+
         ];
 
         return view('products.index', compact('products'));
